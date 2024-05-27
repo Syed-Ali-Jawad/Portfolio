7c28bc93fe9ea333dce628381acc269de5e06777
Syed-Ali-Jawad <alijawad04@gmail.com>
2024-05-27 21:38:12 +0500

    added adjustment

--- src/Sections/MyProjects.jsx
+++ src/Sections/MyProjects.jsx
@@ -1,6 +1,7 @@
 import portfolioProject from "../assets/Portfolio.png";
 import bookStoreProject from "../assets/The Book Store.png";
 import AddProject from "../Components/AddProject";
+import todoProject from "../assets/TodoListProject.png";
 
 export default function MyProjects() {
   return (
@@ -8,14 +9,14 @@ export default function MyProjects() {
       <h1>My Projects</h1>
       <div className="project-cards">
         <AddProject
-          image={bookStoreProject}
-          title="The Book Store"
-          description="A e-commerce store having roles of buyers, sellers and admin. Buyers and sellers can make account for book purcahse and sell while admin can view the book sold by date"
-        />
-        <AddProject
           image={todoProject}
           title="Todo List"
           description="A todo list where you can see statuses of you added tasks, view time left for the task, add an image related to the task."
+        />
+        <AddProject
+          image={bookStoreProject}
+          title="The Book Store"
+          description="A e-commerce store having roles of buyers, sellers and admin. Buyers and sellers can make account for book purcahse and sell while admin can view the book sold by date"
         />
 
         <AddProject
